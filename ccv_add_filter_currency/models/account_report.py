cd924c7d60c315476eda547d74cc1addfe9f1fb7
Đạt ERP <ccv.vn>
2024-11-14 12:46:05 +0700

    update

--- ccv_add_filter_currency/models/account_report.py
+++ ccv_add_filter_currency/models/account_report.py
@@ -32,19 +32,23 @@ class AccountReport(models.Model):
     def _get_lines(self, options, all_column_groups_expression_totals=None):
         self.ensure_one()
         lines = super(AccountReport, self)._get_lines(options, all_column_groups_expression_totals)
-        _logger.info(lines)
+
         currency_id = [currency['id'] for currency in options['currency_options'] if currency['selected']]
+        
         if len(currency_id) > 0:
+            company_currency = self.env.company.currency_id
             currency = self.env['res.currency'].browse(int(currency_id[0]))
-            for line in lines:
-                _logger.info(line['id'])
-                _logger.info(type(line['id']))
-                _logger.info(getattr(line['id']),'currency_id', False)
-                for column in line['columns']:
-                    for column_option in options['columns']:
-                        if column_option.get("figure_type") == "monetary":
-                            if column.get('no_format') is not None:
-                                no_format_value = float(column['no_format']) if isinstance(column['no_format'], (int, float)) else 0.0
-                                column['name'] = currency.format(no_format_value)
-                                column['no_format'] = no_format_value
+            if currency.id != company_currency.id:
+                index_update = []
+                for count, column_option in enumerate(options['columns']):
+                    if column_option.get("figure_type") == "monetary":
+                        index_update.append(count)
+                for line in lines:
+                    for column_index, column in enumerate(line['columns']):
+                        if column_index in index_update and column.get('no_format') is not None:
+                            no_format_value = float(column['no_format'] * currency.rate) if isinstance(column['no_format'], (int, float)) else 0.0
+                            column['name'] = currency.format(no_format_value)
+                            column['no_format'] = no_format_value
+                            
         return lines
+
