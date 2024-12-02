3b06f78204865050b8f1616b91b5f6824a1cddcd
Đạt ERP <ccv.vn>
2024-12-02 13:35:55 +0700

    update

--- ccv_bao_cao/models/hr_expense_sheet.py
+++ ccv_bao_cao/models/hr_expense_sheet.py
@@ -1,11 +1,8 @@
 from odoo import models, fields, api
 import logging
-import datetime
+from datetime import datetime
 from odoo.modules.module import get_module_resource
 from odoo.exceptions import UserError
-
-from ..lib.report import LibraryReport
-libary_report = LibraryReport()
 
 _logger = logging.getLogger(__name__)
 
@@ -13,20 +10,72 @@ class HrExpenseSheet(models.Model):
     _inherit = "hr.expense.sheet"
 
     @api.model
-    def convert_money(self, value):
-        return libary_report.convert_money(value)
+    def convert_money(self, amount):
+        units = ['', 'một', 'hai', 'ba', 'bốn', 'năm', 'sáu', 'bảy', 'tám', 'chín']
+        tens = ['', 'mười', 'hai mươi', 'ba mươi', 'bốn mươi', 'năm mươi', 'sáu mươi', 'bảy mươi', 'tám mươi', 'chín mươi']
+        thousands = ['', 'nghìn', 'triệu', 'tỷ']
+
+        try:
+            cur_amount = int(float(amount))
+        except ValueError:
+            return "Invalid amount"
+
+        def number_to_text(num):
+            if num == 0:
+                return 'không'
+            
+            num = str(num)
+            length = len(num)
+            result = []
+            
+            groups = [num[max(0, length - 3*(i+1)): length - 3*i] for i in range((length // 3) + 1)]
+            groups.reverse()
+            print(num, groups)
+            
+            for idx, group in enumerate(groups):
+                n = int(group)
+                group_result = []
+                
+                if n >= 100:
+                    group_result.append(units[n // 100] + " trăm")
+                    n %= 100
+                if n >= 20:
+                    group_result.append(tens[n // 10])
+                    n %= 10
+                elif n >= 10:
+                    group_result.append('mười')
+                    n %= 10
+                if n > 0:
+                    group_result.append(units[n])
+                
+                if group_result:
+                    result.append(" ".join(group_result) + " " + thousands[len(groups) - idx - 1])
+            
+            return " ".join(result).strip()
+        
+        label = number_to_text(cur_amount).strip()
+
+        return label[:1].upper() + label[1:].lower()
 
     @api.model
-    def convert_vnd(self, value):
-        return libary_report.convert_vnd(value)
+    def convert_vnd(self, amount):
+        return f"{amount:,.0f}"
 
     @api.model
-    def convert_date(self, value):
-        return libary_report.convert_date(value)
+    def convert_date(self, date):
+        if date:
+            return fields.Date.to_string(date)
+        return ''
 
     @api.model
-    def convert_print(self, value):
-        return libary_report.convert_print(value)
+    def convert_print(self, date_value):
+        if not date_value:
+            return ""
+        if isinstance(date_value, datetime):
+            date_value = date_value.date()
+        if isinstance(date_value, fields.Date):
+            date_value = datetime.strptime(str(date_value), '%Y-%m-%d').date()
+        return date_value.strftime('%d/%m/%Y')
 
     partner_bank_id = fields.Many2one('res.partner.bank', string="Đơn vị/Cá nhân Thụ Hưởng")
     payment_type = fields.Selection(string="Hình thức thanh toán", selection=[('cash', 'Tiền mặt'), ('bank', 'Chuyển khoản')])
